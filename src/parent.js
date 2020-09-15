3c2407bb29aff0c0a9cee68b9ca0e2a4a03983ea
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-09-15 09:56:28 -0400

    sfsdsd

---
+++ src/parent.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+var canvas = document.getElementById("signature");
+ var signaturePad = new SignaturePad(canvas);
+
+ $('#clear-signature').on('click', function(){
+     signaturePad.clear();
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
